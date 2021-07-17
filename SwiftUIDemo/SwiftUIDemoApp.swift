d98f220abfbe49a173f2a9f8d8e8720b40b8b908
JunHyuk <nana3993@naver.com>
2021-07-17 12:54:26 +0900

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by june on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
